4a192a6ead5b0fb1b3de3bca6823ca1f606df571
Minah Bae <MBae@lcs.on.ca>
2019-01-19 11:17:03 -0500

    new line art

--- Playground/Playground.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
@@ -4,6 +4,7 @@
 import PlaygroundSupport
 import TortoiseGraphics
 import CoreGraphics
+
 let myFrame = CGRect(x: 0, y: 0, width: 500, height: 500)
 let canvas =
     PlaygroundCanvas(frame:myFrame )
